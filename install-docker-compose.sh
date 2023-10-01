b563ea1b936cced2bd7abee2bd20b74b0049d5dc
sjoh0704 <sjoh0704@daum.net>
2023-10-01 13:58:44 +0900

    update

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,2 +1,3 @@
 curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker-compose --version
